e20d43bc0bdce7e3808835097f8cdb2a10bb9398
souravroy <roy35-909@diu.edu.bd>
2025-02-06 13:06:43 +0600

    changes

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://51.20.8.202:5001', // Adjust to your backend URL
+  baseURL: 'http://13.60.49.35:5001', // Adjust to your backend URL
   headers: { 'Content-Type': 'application/json' },
 });
 
